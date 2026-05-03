2fca3b510a2f6d9a578bad4b0e2851ed72d6feff
Bavannarayana Kavarthapu <k.bavannarayana@gmail.com>
2026-05-03 19:17:52 +0530

    bug fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
               <div>
                 <p className="text-sm text-zinc-400">Email</p>
                 <a
-                  href="mailtok.bavannarayana@gmail.com"
+                  href="mailto:k.bavannarayana@gmail.com"
                   className="text-white hover:text-zinc-300"
                 >
                   k.bavannarayana@gmail.com
