f1781a3a16715cf9946edebf360dc7afe959730f
Bavannarayana Kavarthapu <k.bavannarayana@gmail.com>
2026-05-03 09:42:47 +0530

    small data fix

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -41,7 +41,7 @@ export const projects = [
   {
     title: "Travel List",
     desc: "Interactive travel planning app to manage packing items, track progress, and organize trips efficiently. Built with dynamic state management and responsive component-based architecture.",
-    image: habitTracker,
+    image: travelList,
     tech: ["React", "Javascript"],
     live: "https://travelistt.netlify.app/",
     github: "https://github.com/bavannarayana/travel-list",
@@ -49,7 +49,7 @@ export const projects = [
   {
     title: "Habit Tracker",
     desc: "Productivity app for tracking daily habits, monitoring consistency, and building routines over time. Designed clean dashboards and intuitive UI using React, TypeScript, and Material UI.",
-    image: travelList,
+    image: habitTracker,
     tech: ["React", "RTK", "Material UI"],
     live: "https://track-daily-habits.netlify.app/",
     github: "https://github.com/bavannarayana/habit-tracker",
